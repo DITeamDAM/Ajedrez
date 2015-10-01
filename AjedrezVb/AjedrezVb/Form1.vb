e9b250bfd3fe4a1a358ae7a356a07e9ad5a2d912
PacoMC <paco.marchante11@gmail.com>
2015-10-01 12:55:16 +0200

    Figuras 2.0

--- AjedrezVb/AjedrezVb/Form1.vb
+++ AjedrezVb/AjedrezVb/Form1.vb
@@ -8,171 +8,7 @@
         For i = 0 To 7 'i --> recorre x
 
             For j = 0 To 7 'j --> recorre y
-                Dim pieza As New PictureBox
-                With pieza
-                    SetStyle(ControlStyles.SupportsTransparentBackColor, True)
 
-                    If i = 0 And (j = 0 Or j = 7) Then
-
-
-                        .Name = "pn_torre"
-                        .Size = New System.Drawing.Size(75, 75)
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/tn.png")
-                        Me.Controls.Add(pieza)
-                    End If
-                    If i = 0 And (j = 1 Or j = 6) Then
-
-                        .Name = "pn_caballo"
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/cn.png")
-                        Me.Controls.Add(pieza)
-                    End If
-                    If i = 0 And (j = 2 Or j = 5) Then
-
-                        .Name = "pn_alfil"
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/an.png")
-                        Me.Controls.Add(pieza)
-                    End If
-                    If i = 0 And (j = 3) Then
-
-                        .Name = "pn_reina"
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/qn.png")
-                        Me.Controls.Add(pieza)
-                    End If
-                    If i = 0 And (j = 4) Then
-
-                        .Name = "pn_rey"
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/kn.png")
-                        Me.Controls.Add(pieza)
-                    End If
-                    If i = 1 Then
-
-                        .Name = "pn_peon" & j
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/pn.png")
-                        Me.Controls.Add(pieza)
-                    End If
-
-
-                    If i = 7 And (j = 0 Or j = 7) Then
-
-                        .Name = "pb_torre"
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/tb.png")
-                        Me.Controls.Add(pieza)
-                    End If
-                    If i = 7 And (j = 1 Or j = 6) Then
-
-                        .Name = "pb_caballo"
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/cb.png")
-                        Me.Controls.Add(pieza)
-                    End If
-                    If i = 7 And (j = 2 Or j = 5) Then
-
-                        .Name = "pb_alfil"
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/ab.png")
-                        Me.Controls.Add(pieza)
-                    End If
-                    If i = 7 And (j = 3) Then
-
-                        .Name = "pb_reina"
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/qb.png")
-                        Me.Controls.Add(pieza)
-                    End If
-                    If i = 7 And (j = 4) Then
-
-                        .Name = "pb_rey"
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/kb.png")
-                        Me.Controls.Add(pieza)
-                    End If
-                    If i = 6 Then
-
-                        .Name = "pb_torre"
-                        .Size = New System.Drawing.Size(75, 75)
-
-
-                        .BorderStyle = BorderStyle.FixedSingle
-                        .SizeMode = PictureBoxSizeMode.StretchImage
-
-                        .Location = New System.Drawing.Point(x, y)
-                        pieza.Load(Application.StartupPath & "/images/pb.png")
-                        Me.Controls.Add(pieza)
-
-                    End If
-
-                End With
 
 
 
@@ -185,6 +21,8 @@
                     .BorderStyle = BorderStyle.FixedSingle
                     .SizeMode = PictureBoxSizeMode.StretchImage
                     .Tag = 0
+
+
 
 
                     If i Mod 2 = 0 Then
@@ -200,11 +38,65 @@
                         End If
                     End If
 
+                    If i = 1 Then
+                        .Tag = 11
+                        casilla.Load(Application.StartupPath & "/images/pn.png")
+                    End If
+                    If i = 0 And (j = 0 Or j = 7) Then
+                        .Tag = 12
+                        casilla.Load(Application.StartupPath & "/images/tn.png")
+                    End If
+                    If i = 0 And (j = 1 Or j = 6) Then
+                        .Tag = 13
+                        casilla.Load(Application.StartupPath & "/images/cn.png")
+                    End If
+                    If i = 0 And (j = 2 Or j = 5) Then
+                        .Tag = 14
+                        casilla.Load(Application.StartupPath & "/images/an.png")
+                    End If
+                    If i = 0 And (j = 3) Then
+                        .Tag = 15
+                        casilla.Load(Application.StartupPath & "/images/qn.png")
+                    End If
+                    If i = 0 And (j = 4) Then
+                        .Tag = 14
+                        casilla.Load(Application.StartupPath & "/images/kn.png")
+                    End If
+
+
+                    If i = 6 Then
+                        .Tag = 11
+                        casilla.Load(Application.StartupPath & "/images/pb.png")
+                    End If
+                    If i = 7 And (j = 0 Or j = 7) Then
+                        .Tag = 12
+                        casilla.Load(Application.StartupPath & "/images/tb.png")
+                    End If
+                    If i = 7 And (j = 1 Or j = 6) Then
+                        .Tag = 13
+                        casilla.Load(Application.StartupPath & "/images/cb.png")
+                    End If
+                    If i = 7 And (j = 2 Or j = 5) Then
+                        .Tag = 14
+                        casilla.Load(Application.StartupPath & "/images/ab.png")
+                    End If
+                    If i = 7 And (j = 3) Then
+                        .Tag = 15
+                        casilla.Load(Application.StartupPath & "/images/qb.png")
+                    End If
+                    If i = 7 And (j = 4) Then
+                        .Tag = 14
+                        casilla.Load(Application.StartupPath & "/images/kb.png")
+                    End If
+
 
                     Me.Controls.Add(casilla)
 
 
+
                 End With
+
+
 
                 x += 75
 
