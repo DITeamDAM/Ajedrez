0a93007b2fb15b09a24b221035a007f400972c11
Jaimerocel96 <jaimerocel96@gmail.com>
2015-10-03 19:05:58 +0200

    Movimiento torre + Interponer

--- AjedrezVb/AjedrezVb/Form1.vb
+++ AjedrezVb/AjedrezVb/Form1.vb
@@ -72,33 +72,33 @@
                         casilla.Load(Application.StartupPath & "/images/qn.png")
                     End If
                     If i = 0 And (j = 4) Then
-                        .Tag = 14
+                        .Tag = 16
                         casilla.Load(Application.StartupPath & "/images/kn.png")
                     End If
 
                     'Aqui las blancas
                     If i = 6 Then
-                        .Tag = 11
+                        .Tag = 21
                         casilla.Load(Application.StartupPath & "/images/pb.png")
                     End If
                     If i = 7 And (j = 0 Or j = 7) Then
-                        .Tag = 12
+                        .Tag = 22
                         casilla.Load(Application.StartupPath & "/images/tb.png")
                     End If
                     If i = 7 And (j = 1 Or j = 6) Then
-                        .Tag = 13
+                        .Tag = 23
                         casilla.Load(Application.StartupPath & "/images/cb.png")
                     End If
                     If i = 7 And (j = 2 Or j = 5) Then
-                        .Tag = 14
+                        .Tag = 24
                         casilla.Load(Application.StartupPath & "/images/ab.png")
                     End If
                     If i = 7 And (j = 3) Then
-                        .Tag = 15
+                        .Tag = 25
                         casilla.Load(Application.StartupPath & "/images/qb.png")
                     End If
                     If i = 7 And (j = 4) Then
-                        .Tag = 14
+                        .Tag = 26
                         casilla.Load(Application.StartupPath & "/images/kb.png")
                     End If
                     'Hasta aqui se posicionan
@@ -123,26 +123,66 @@
     End Sub
 
 
-
+    Dim tagFigura As String
 
     Private Sub Colocar(sender As Object, e As EventArgs)
 
 
 
         If (sender.Tag <> "0") Then                                                                                 'Si la posicion que pulsas es distinto de 0, es una figura
+
             If figura Is Nothing Then                                                                               'Si no tenemos figura guardada (null) --> la guarda
                 figura = sender                                                                                     'Guardamos la figura
-                MsgBox("Figura guardada: " & figura.Name() & " Tag: " & figura.Tag())
+                tagFigura = figura.Tag
+                tagFigura = tagFigura.Substring(0, 1)
+                MsgBox("Figura guardada: " & figura.Name() & " Tag: " & figura.Tag() & " TEAM: " & tagFigura)
                 Color_Inicio = figura.BackColor                                                                     'Guardamos el color del tablero de la posicion de la figura
                 figura.BackColor = Color.Red                                                                        'Resaltamos la figura seleccionada en ese momento
-            Else                                                                                                    'Si ya hay una figura seleccionada --> Teniendo una figura, Pulsas en otra figura. Comprobar si la figura que pulsas es una figura propia --> error, una figura enemiga --> comer
-                MsgBox("Ya tienes una figura guardada")
+                'Si ya hay una figura seleccionada --> Teniendo una figura, Pulsas en otra figura. Comprobar si la figura que pulsas es una figura propia --> error, una figura enemiga --> comer
+
+
+
                 'Insertar funcion para detectar si es del equipo contrario --> Comer; sino --> no deja moverse
+            End If
+            'calcula los tag de la nueva figura
+            Dim tagSender As String
+            tagSender = sender.tag
+            tagSender = tagSender.Substring(0, 1)
+
+            'controlador para cambiar  de figura
+            If tagFigura = tagSender Then
+                'Guardamos la figura
+                figura.BackColor = Color_Inicio
+                figura = sender
+                tagFigura = figura.Tag
+                tagFigura = tagFigura.Substring(0, 1)
+                MsgBox("Figura guardada: " & figura.Name() & " Tag: " & figura.Tag() & " TEAM: " & tagFigura)
+                Color_Inicio = figura.BackColor                                                                     'Guardamos el color del tablero de la posicion de la figura
+                figura.BackColor = Color.Red
+            End If
+            'controlador para cuando seleccionamos una figura de otro equipo
+            If tagFigura <> tagSender Then
+                If comprobador(figura, sender) Then
+                    MsgBox("Moviendo figura")
+                    Color_Final = sender.BackColor                                                                      'Guardamos el color de la posicion a la que nos movemos
+                    sender.BackColor = Color.Green                                                                      'Seteamos el color de la posicion a verde (creo que se puede prescindir, abajo lo volvemos a poner al default...)
+                    sender.Tag = figura.Tag                                                                             'Establecemos el tag de la imagen a la posicion que hemos pulsado (la figura se mueve y ocupa la posicion vacia)
+                    figura.Tag = "0"                                                                                    'Establecemos la posicion donde estaba la figura a 0 (la figura se mueve y deja libre su posicion inicial)
+                    MsgBox(sender.Tag)
+                    sender.ImageLocation = figura.ImageLocation                                                         'Establecemos la imagen de la posicion vacia a la imagen de la figura que se mueve
+                    figura.ImageLocation = Nothing                                                                      'Quitamos la imagen de la figura de su posicion inicial (null)
+                    figura.BackColor = Color_Inicio                                                                     'Establecemos el color de fondo de la posicion inicial a la del tablero
+                    sender.BackColor = Color_Final                                                                      'Establecemos el color de la posicion a moverse a la del tablero (Creo que se puede prescindir esto junto al verde)
+                    figura = Nothing
+                End If
+
             End If
 
         Else                                                                                                        'Posicion pulsada es 0 --> Si hay figura --> moverse; sino --> Error --> Pinchar primero en una figura
-            If figura IsNot Nothing Then                                                                            'Si tienes figura guardada (no null) --> Movemos la figura
-
+            'Si tienes figura guardada (no null) --> Movemos la figura
+            If figura Is Nothing Then
+                MsgBox("Pincha primero en una figura")
+            Else
                 If comprobador(figura, sender) Then
 
                     MsgBox("Moviendo figura")
@@ -156,10 +196,12 @@
                     figura.BackColor = Color_Inicio                                                                     'Establecemos el color de fondo de la posicion inicial a la del tablero
                     sender.BackColor = Color_Final                                                                      'Establecemos el color de la posicion a moverse a la del tablero (Creo que se puede prescindir esto junto al verde)
                     figura = Nothing                                                                                    'Resetamos la figura seleccionada para el siguiente movimiento
-                Else                                                                                                    'No hay figura guardada --> Error --> Pinchar primero una figura
-                    MsgBox("Pincha primero en una figura")
+                    'No hay figura guardada --> Error --> Pinchar primero una figura
+
                 End If
             End If
+
+
 
 
         End If
@@ -168,25 +210,27 @@
 
     End Sub
 
-
+    'filtro de figuras y sus movimientos
     Function comprobador(ByVal figura As PictureBox, ByVal nCasilla As PictureBox)
         MsgBox(figura.Tag & "ESTO ES LA FUNCION COMPROBAR")
         Select Case figura.Tag
             Case 11
-                Return MovPeon11(figura, nCasilla)
+
             Case 12
-                Return True
+                Return MovTorre21(figura, nCasilla)
             Case 13
             Case 14
             Case 15
             Case 16
             Case 21
+                Return MovPeon11(figura, nCasilla)
             Case 22
+                Return MovTorre21(figura, nCasilla)
             Case 23
             Case 24
             Case 25
             Case 26
-
+                Return MovRey26(figura, nCasilla)
 
 
         End Select
@@ -195,13 +239,14 @@
 
 
     End Function
+    'Movimiento del peon
 
     Function MovPeon11(ByVal peon As PictureBox, ByVal nuevaCasilla As PictureBox)
         Dim posInicial As String
         Dim posFinal As String
         Dim inicio As String
 
-        posInicial = peon.Name
+        posInicial = peon.Name                          'Calcula las posiciones tanto del inicio como del final
         posInicial = posInicial.Substring(7)
         posFinal = nuevaCasilla.Name
         posFinal = posFinal.Substring(7)
@@ -209,6 +254,7 @@
 
         MsgBox(posInicial & " ,pos final: " & posFinal)
 
+        'condicion para primer movimiento del peon
         If inicio = 6 Then
             If posFinal = posInicial - 10 Then
                 Return True
@@ -219,14 +265,29 @@
 
             End If
         Else
+            'controlador del movimiento del peon
             If posFinal = posInicial - 10 Then
-                If nuevaCasilla.Tag > 20 Then
+                If nuevaCasilla.Tag > 10 Then
                     Return False
                 Else
                     Return True
                 End If
 
-
+            End If
+            'controlador como come el peon
+            If posFinal = posInicial - 11 Then
+                If nuevaCasilla.Tag > 10 Then
+                    Return True
+                Else
+                    Return False
+                End If
+            End If
+            If posFinal = posInicial - 9 Then
+                If nuevaCasilla.Tag > 10 Then
+                    Return True
+                Else
+                    Return False
+                End If
             End If
 
         End If
@@ -234,6 +295,121 @@
 
     End Function
 
+
+    Function MovTorre21(ByVal torre As PictureBox, ByVal nuevaCasilla As PictureBox)
+        Dim posVertical_Inicial As String
+        Dim posVertical_Final As String
+        Dim posHorizontal_Inicial As String
+        Dim posHorizontal_Final As String
+
+        posVertical_Inicial = torre.Name                          'Calcula las posiciones tanto del inicio como del final
+        posVertical_Inicial = posVertical_Inicial.Substring(8)
+        posVertical_Final = nuevaCasilla.Name
+        posVertical_Final = posVertical_Final.Substring(8)
+
+
+        posHorizontal_Inicial = torre.Name                          'Calcula las posiciones tanto del inicio como del final
+        posHorizontal_Inicial = Mid(posHorizontal_Inicial, 8, 1)
+        posHorizontal_Final = nuevaCasilla.Name
+        posHorizontal_Final = Mid(posHorizontal_Final, 8, 1)
+
+        MsgBox("TORRE NAME: " & torre.Name)
+        MsgBox("CASILLA NAME: " & nuevaCasilla.Name)
+
+        'MsgBox(posVertical_Inicial & " ,pos final: " & posVertical_Final)
+        MsgBox(posHorizontal_Inicial & " ,pos final: " & posHorizontal_Final)
+
+        If posVertical_Inicial = posVertical_Final Then
+            Return True
+        End If
+
+        If posHorizontal_Inicial = posHorizontal_Final Then
+            Return True
+        End If
+
+
+
+
+    End Function
+
+
+    Function Interponer(ByVal figura As PictureBox, ByVal nuevaCasilla As PictureBox)
+
+
+        Dim posInicial As String
+        Dim posFinal As String
+
+
+        Dim posVertical_Inicial As String
+        Dim posVertical_Final As String
+        Dim posHorizontal_Inicial As String
+        Dim posHorizontal_Final As String
+
+
+
+        posInicial = figura.Name                          'Calcula las posiciones tanto del inicio como del final
+        posInicial = posInicial.Substring(7)
+        posFinal = nuevaCasilla.Name
+        posFinal = posFinal.Substring(7)
+
+
+
+        For contador = posInicial To posFinal Step 10
+
+        Next
+
+
+    End Function
+
+    Function MovRey26(ByVal rey As PictureBox, ByVal nuevaCasilla As PictureBox)
+        Dim posInicial As String
+        Dim posFinal As String
+        Dim inicio As String
+
+        posInicial = rey.Name                          'Calcula las posiciones tanto del inicio como del final
+        posInicial = posInicial.Substring(7)
+        posFinal = nuevaCasilla.Name
+        posFinal = posFinal.Substring(7)
+        inicio = posInicial.Substring(0, 1)
+
+        MsgBox(posInicial & " ,pos final: " & posFinal)
+        ' controlador de movimiento horizontal
+        If posFinal = posInicial - 1 Or posFinal = posInicial + 1 Then
+            If nuevaCasilla.Tag > 10 And nuevaCasilla.Tag < 17 Then
+                Return True
+            End If
+            If nuevaCasilla.Tag > 20 Then
+                Return False
+            Else
+                Return True
+            End If
+
+        End If
+        'controlador de movimiento vertical
+        If posFinal = posInicial + 10 Or posFinal = posInicial - 10 Then
+            If nuevaCasilla.Tag > 10 And nuevaCasilla.Tag < 17 Then
+                Return True
+            End If
+            If nuevaCasilla.Tag > 20 Then
+                Return False
+            Else
+                Return True
+            End If
+        End If
+        'controlador de movimiento diagonal
+        If posFinal = posInicial + 9 Or posFinal = posInicial + 11 Or posFinal = posInicial - 9 Or posFinal = posInicial - 11 Then
+            If nuevaCasilla.Tag > 10 And nuevaCasilla.Tag < 17 Then
+                Return True
+            End If
+            If nuevaCasilla.Tag > 20 Then
+                Return False
+            Else
+                Return True
+            End If
+
+        End If
+
+    End Function
 
 
 
