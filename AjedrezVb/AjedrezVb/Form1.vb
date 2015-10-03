ab0ed54afee095f8f1502def7997ebc3717aae4f
PacoMC <paco.marchante11@gmail.com>
2015-10-03 17:16:15 +0200

    Controlador con peon terminado

--- AjedrezVb/AjedrezVb/Form1.vb
+++ AjedrezVb/AjedrezVb/Form1.vb
@@ -123,33 +123,69 @@
     End Sub
 
 
-
+    Dim tagFigura As String
 
     Private Sub Colocar(sender As Object, e As EventArgs)
 
 
 
         If (sender.Tag <> "0") Then                                                                                 'Si la posicion que pulsas es distinto de 0, es una figura
-            Dim tagFigura As String
 
             If figura Is Nothing Then                                                                               'Si no tenemos figura guardada (null) --> la guarda
                 figura = sender                                                                                     'Guardamos la figura
                 tagFigura = figura.Tag
-                MsgBox("Figura guardada: " & figura.Name() & " Tag: " & figura.Tag())
+                tagFigura = tagFigura.Substring(0, 1)
+                MsgBox("Figura guardada: " & figura.Name() & " Tag: " & figura.Tag() & " TEAM: " & tagFigura)
                 Color_Inicio = figura.BackColor                                                                     'Guardamos el color del tablero de la posicion de la figura
                 figura.BackColor = Color.Red                                                                        'Resaltamos la figura seleccionada en ese momento
-            Else                                                                                                    'Si ya hay una figura seleccionada --> Teniendo una figura, Pulsas en otra figura. Comprobar si la figura que pulsas es una figura propia --> error, una figura enemiga --> comer
+                'Si ya hay una figura seleccionada --> Teniendo una figura, Pulsas en otra figura. Comprobar si la figura que pulsas es una figura propia --> error, una figura enemiga --> comer
 
-                MsgBox("Ya tienes una figura guardada")
+
+
                 'Insertar funcion para detectar si es del equipo contrario --> Comer; sino --> no deja moverse
+            End If
+            'calcula los tag de la nueva figura
+            Dim tagSender As String
+            tagSender = sender.tag
+            tagSender = tagSender.Substring(0, 1)
+
+            'controlador para cambiar  de figura
+            If tagFigura = tagSender Then
+                'Guardamos la figura
+                figura.BackColor = Color_Inicio
+                figura = sender
+                tagFigura = figura.Tag
+                tagFigura = tagFigura.Substring(0, 1)
+                MsgBox("Figura guardada: " & figura.Name() & " Tag: " & figura.Tag() & " TEAM: " & tagFigura)
+                Color_Inicio = figura.BackColor                                                                     'Guardamos el color del tablero de la posicion de la figura
+                figura.BackColor = Color.Red
+            End If
+            'controlador para cuando seleccionamos una figura de otro equipo
+            If tagFigura <> tagSender Then
+                If comprobador(figura, sender) Then
+                    MsgBox("Moviendo figura")
+                    Color_Final = sender.BackColor                                                                      'Guardamos el color de la posicion a la que nos movemos
+                    sender.BackColor = Color.Green                                                                      'Seteamos el color de la posicion a verde (creo que se puede prescindir, abajo lo volvemos a poner al default...)
+                    sender.Tag = figura.Tag                                                                             'Establecemos el tag de la imagen a la posicion que hemos pulsado (la figura se mueve y ocupa la posicion vacia)
+                    figura.Tag = "0"                                                                                    'Establecemos la posicion donde estaba la figura a 0 (la figura se mueve y deja libre su posicion inicial)
+                    MsgBox(sender.Tag)
+                    sender.ImageLocation = figura.ImageLocation                                                         'Establecemos la imagen de la posicion vacia a la imagen de la figura que se mueve
+                    figura.ImageLocation = Nothing                                                                      'Quitamos la imagen de la figura de su posicion inicial (null)
+                    figura.BackColor = Color_Inicio                                                                     'Establecemos el color de fondo de la posicion inicial a la del tablero
+                    sender.BackColor = Color_Final                                                                      'Establecemos el color de la posicion a moverse a la del tablero (Creo que se puede prescindir esto junto al verde)
+                    figura = Nothing
+                End If
+
             End If
 
         Else                                                                                                        'Posicion pulsada es 0 --> Si hay figura --> moverse; sino --> Error --> Pinchar primero en una figura
-            If comprobador(figura, sender) Then                                                                          'Si tienes figura guardada (no null) --> Movemos la figura
+            'Si tienes figura guardada (no null) --> Movemos la figura
+            If figura Is Nothing Then
+                MsgBox("Pincha primero en una figura")
+            Else
+                If comprobador(figura, sender) Then
 
-
-
-                MsgBox("Moviendo figura")
+                    MsgBox("Moviendo figura")
                     Color_Final = sender.BackColor                                                                      'Guardamos el color de la posicion a la que nos movemos
                     sender.BackColor = Color.Green                                                                      'Seteamos el color de la posicion a verde (creo que se puede prescindir, abajo lo volvemos a poner al default...)
                     sender.Tag = figura.Tag                                                                             'Establecemos el tag de la imagen a la posicion que hemos pulsado (la figura se mueve y ocupa la posicion vacia)
@@ -160,10 +196,12 @@
                     figura.BackColor = Color_Inicio                                                                     'Establecemos el color de fondo de la posicion inicial a la del tablero
                     sender.BackColor = Color_Final                                                                      'Establecemos el color de la posicion a moverse a la del tablero (Creo que se puede prescindir esto junto al verde)
                     figura = Nothing                                                                                    'Resetamos la figura seleccionada para el siguiente movimiento
-                Else                                                                                                    'No hay figura guardada --> Error --> Pinchar primero una figura
-                    MsgBox("Pincha primero en una figura")
+                    'No hay figura guardada --> Error --> Pinchar primero una figura
 
+                End If
             End If
+
+
 
 
         End If
@@ -172,12 +210,12 @@
 
     End Sub
 
-
+    'filtro de figuras y sus movimientos
     Function comprobador(ByVal figura As PictureBox, ByVal nCasilla As PictureBox)
         MsgBox(figura.Tag & "ESTO ES LA FUNCION COMPROBAR")
         Select Case figura.Tag
             Case 11
-                Return MovPeon11(figura, nCasilla)
+
             Case 12
                 Return True
             Case 13
@@ -185,11 +223,14 @@
             Case 15
             Case 16
             Case 21
+                Return MovPeon11(figura, nCasilla)
             Case 22
             Case 23
             Case 24
             Case 25
             Case 26
+            Case 0
+                MsgBox("Selecciona una figura")
 
 
 
@@ -199,13 +240,14 @@
 
 
     End Function
+    'Movimiento del peon
 
     Function MovPeon11(ByVal peon As PictureBox, ByVal nuevaCasilla As PictureBox)
         Dim posInicial As String
         Dim posFinal As String
         Dim inicio As String
 
-        posInicial = peon.Name
+        posInicial = peon.Name                          'Calcula las posiciones tanto del inicio como del final
         posInicial = posInicial.Substring(7)
         posFinal = nuevaCasilla.Name
         posFinal = posFinal.Substring(7)
@@ -213,6 +255,7 @@
 
         MsgBox(posInicial & " ,pos final: " & posFinal)
 
+        'condicion para primer movimiento del peon
         If inicio = 6 Then
             If posFinal = posInicial - 10 Then
                 Return True
@@ -223,14 +266,29 @@
 
             End If
         Else
+            'controlador del movimiento del peon
             If posFinal = posInicial - 10 Then
-                If nuevaCasilla.Tag > 20 Then
+                If nuevaCasilla.Tag > 10 Then
                     Return False
                 Else
                     Return True
                 End If
 
-
+            End If
+            'controlador como come el peon
+            If posFinal = posInicial - 11 Then
+                If nuevaCasilla.Tag > 10 Then
+                    Return True
+                Else
+                    Return False
+                End If
+            End If
+            If posFinal = posInicial - 9 Then
+                If nuevaCasilla.Tag > 10 Then
+                    Return True
+                Else
+                    Return False
+                End If
             End If
 
         End If
