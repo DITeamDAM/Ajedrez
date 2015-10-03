e7026598cb4820ce82f7270aace826e300fde396
Jaimerocel96 <jaimerocel96@gmail.com>
2015-10-03 13:16:12 +0200

    Arreglo variables puto luis y parte de la colocacion

--- AjedrezVb/AjedrezVb/Form1.vb
+++ AjedrezVb/AjedrezVb/Form1.vb
@@ -1,21 +1,29 @@
 ﻿Public Class Form1
+
+    Dim figura As PictureBox
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim x As Integer 'posicion en x
         Dim y As Integer 'posicion en y
 
 
 
-        For x = 0 To 7 'x --> recorre x
 
-            For y = 0 To 7 'y --> recorre y
+        For i = 0 To 7 'i --> recorre x
+
+            For j = 0 To 7 'j --> recorre y
 
 
-                'LUIS -> Les he cambiado el nombre a las vars por x e y (Y)
+                'Jaime --> No cambiar las variables i-j por x-y porque bugea el bucle y adios form.
+                'Si quereis hacer multicomentario seleccionar lo que quereis comentar y --> (Ctr+K y Ctr+C);
+                'Para quitarlo (Ctr+K y Ctr+U); De nada :D
+
+
 
                 Dim casilla As New PictureBox
                 With casilla
 
-                    .Name = "Casilla" & x & y
+                    .Name = "Casilla" & i & j
                     .Size = New System.Drawing.Size(75, 75)
                     .Location = New System.Drawing.Point(x, y)
                     .BorderStyle = BorderStyle.FixedSingle
@@ -25,15 +33,15 @@
 
 
                     'Se diferencia el color de las casillas
-                    If x Mod 2 = 0 Then
+                    If i Mod 2 = 0 Then
                         .BackColor = Color.WhiteSmoke
-                        If y Mod 2 <> 0 Then
+                        If j Mod 2 <> 0 Then
                             .BackColor = Color.BurlyWood
                         End If
                     Else
                         '.BackColor = Color.Black
                         .BackColor = Color.BurlyWood
-                        If y Mod 2 <> 0 Then
+                        If j Mod 2 <> 0 Then
                             .BackColor = Color.WhiteSmoke
                         End If
                     End If
@@ -41,53 +49,53 @@
 
                     'Se posicionan las piezas en los PictureBox
                     'Aqui las negras
-                    If x = 1 Then
+                    If i = 1 Then
                         .Tag = 11
                         casilla.Load(Application.StartupPath & "/images/pn.png")
                     End If
-                    If x = 0 And (y = 0 Or y = 7) Then
+                    If i = 0 And (j = 0 Or j = 7) Then
                         .Tag = 12
                         casilla.Load(Application.StartupPath & "/images/tn.png")
                     End If
-                    If x = 0 And (y = 1 Or y = 6) Then
+                    If i = 0 And (j = 1 Or j = 6) Then
                         .Tag = 13
                         casilla.Load(Application.StartupPath & "/images/cn.png")
                     End If
-                    If x = 0 And (y = 2 Or y = 5) Then
+                    If i = 0 And (j = 2 Or j = 5) Then
                         .Tag = 14
                         casilla.Load(Application.StartupPath & "/images/an.png")
                     End If
-                    If x = 0 And (y = 3) Then
+                    If i = 0 And (j = 3) Then
                         .Tag = 15
                         casilla.Load(Application.StartupPath & "/images/qn.png")
                     End If
-                    If x = 0 And (y = 4) Then
+                    If i = 0 And (j = 4) Then
                         .Tag = 14
                         casilla.Load(Application.StartupPath & "/images/kn.png")
                     End If
 
                     'Aqui las blancas
-                    If x = 6 Then
+                    If i = 6 Then
                         .Tag = 11
                         casilla.Load(Application.StartupPath & "/images/pb.png")
                     End If
-                    If x = 7 And (y = 0 Or y = 7) Then
+                    If i = 7 And (j = 0 Or j = 7) Then
                         .Tag = 12
                         casilla.Load(Application.StartupPath & "/images/tb.png")
                     End If
-                    If x = 7 And (y = 1 Or y = 6) Then
+                    If i = 7 And (j = 1 Or j = 6) Then
                         .Tag = 13
                         casilla.Load(Application.StartupPath & "/images/cb.png")
                     End If
-                    If x = 7 And (y = 2 Or y = 5) Then
+                    If i = 7 And (j = 2 Or j = 5) Then
                         .Tag = 14
                         casilla.Load(Application.StartupPath & "/images/ab.png")
                     End If
-                    If x = 7 And (y = 3) Then
+                    If i = 7 And (j = 3) Then
                         .Tag = 15
                         casilla.Load(Application.StartupPath & "/images/qb.png")
                     End If
-                    If x = 7 And (y = 4) Then
+                    If i = 7 And (j = 4) Then
                         .Tag = 14
                         casilla.Load(Application.StartupPath & "/images/kb.png")
                     End If
@@ -118,16 +126,51 @@
     Private Sub Colocar(sender As Object, e As EventArgs)
 
 
-        Dim figura As PictureBox
+
         Dim pos_Inicio As Integer
         Dim pos_Fin As Integer
 
-        If (figura.Name = "") Then
-            MsgBox("nulo")
+
+        'Primero check if figura -> guardada; Si -> seleccionar posicion; No --> pincha en una figura;
+
+
+
+
+
+        If (sender.Tag <> "0") Then
+            If figura Is Nothing Then
+                figura = sender
+                MsgBox("Figura guardada: " & figura.Name() & " Tag: " & figura.Tag())
+                figura.BackColor = Color.Red
+            Else
+                MsgBox("Ya tienes una figura guardada")
+            End If
+
+
+
+
         End If
 
 
-        MsgBox("Nombre: " & sender.Name & " Tag: " & sender.Tag)
+
+
+
+        'If figura IsNot Nothing Then
+        '    MsgBox("Ya tienes seleccionada una figura, selecciona una posicion para moverla")
+        'Else
+        '    If (sender.Tag <> "0") Then
+        '        figura = sender
+        '        MsgBox("Figura guardada:" & figura.Name() & " " & figura.Tag())
+        '    Else
+        '        If figura Is Nothing Then
+        '            MsgBox("Tienes que pinchar primero en una figura")
+        '        End If
+
+        '    End If
+        'End If
+
+
+        'MsgBox("Nombre: " & sender.Name & " Tag: " & sender.Tag)
 
 
         'prueba master jaime
