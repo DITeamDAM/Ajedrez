6d258a52d2ab3185c3796aabdce030e06c0e9ea5
PacoMC <paco.marchante11@gmail.com>
2015-10-03 15:57:54 +0200

    Ultima version

--- AjedrezVb/AjedrezVb/Form1.vb
+++ AjedrezVb/AjedrezVb/Form1.vb
@@ -1,7 +1,13 @@
 ﻿Public Class Form1
+
+    Dim figura As PictureBox
+    Dim Color_Inicio As Color
+    Dim Color_Final As Color
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim x As Integer 'posicion en x
         Dim y As Integer 'posicion en y
+
 
 
 
@@ -9,6 +15,10 @@
 
             For j = 0 To 7 'j --> recorre y
 
+
+                'Jaime --> No cambiar las variables i-j por x-y porque bugea el bucle y adios form.
+                'Si quereis hacer multicomentario seleccionar lo que quereis comentar y --> (Ctr+K y Ctr+C);
+                'Para quitarlo (Ctr+K y Ctr+U); De nada :D
 
 
 
@@ -24,7 +34,7 @@
 
 
 
-
+                    'Se diferencia el color de las casillas
                     If i Mod 2 = 0 Then
                         .BackColor = Color.WhiteSmoke
                         If j Mod 2 <> 0 Then
@@ -38,6 +48,9 @@
                         End If
                     End If
 
+
+                    'Se posicionan las piezas en los PictureBox
+                    'Aqui las negras
                     If i = 1 Then
                         .Tag = 11
                         casilla.Load(Application.StartupPath & "/images/pn.png")
@@ -63,7 +76,7 @@
                         casilla.Load(Application.StartupPath & "/images/kn.png")
                     End If
 
-
+                    'Aqui las blancas
                     If i = 6 Then
                         .Tag = 11
                         casilla.Load(Application.StartupPath & "/images/pb.png")
@@ -88,14 +101,10 @@
                         .Tag = 14
                         casilla.Load(Application.StartupPath & "/images/kb.png")
                     End If
-
+                    'Hasta aqui se posicionan
 
                     Me.Controls.Add(casilla)
-
-
                     AddHandler casilla.MouseClick, AddressOf Colocar
-
-
 
                 End With
 
@@ -118,16 +127,114 @@
 
     Private Sub Colocar(sender As Object, e As EventArgs)
 
-        MsgBox("Nombre: " & sender.Name & " Tag: " & sender.Tag)
 
-        MsgBox("Nombre: " & sender.Name & " Tag: " & sender.Tag)
 
-        MsgBox("Nombre: " & sender.Name & " Tag: " & sender.Tag)
+        If (sender.Tag <> "0") Then                                                                                 'Si la posicion que pulsas es distinto de 0, es una figura
+            If figura Is Nothing Then                                                                               'Si no tenemos figura guardada (null) --> la guarda
+                figura = sender                                                                                     'Guardamos la figura
+                MsgBox("Figura guardada: " & figura.Name() & " Tag: " & figura.Tag())
+                Color_Inicio = figura.BackColor                                                                     'Guardamos el color del tablero de la posicion de la figura
+                figura.BackColor = Color.Red                                                                        'Resaltamos la figura seleccionada en ese momento
+            Else                                                                                                    'Si ya hay una figura seleccionada --> Teniendo una figura, Pulsas en otra figura. Comprobar si la figura que pulsas es una figura propia --> error, una figura enemiga --> comer
+                MsgBox("Ya tienes una figura guardada")
+                'Insertar funcion para detectar si es del equipo contrario --> Comer; sino --> no deja moverse
+            End If
 
-        'la rama der paco
+        Else                                                                                                        'Posicion pulsada es 0 --> Si hay figura --> moverse; sino --> Error --> Pinchar primero en una figura
+            If figura IsNot Nothing Then                                                                            'Si tienes figura guardada (no null) --> Movemos la figura
+
+                If comprobador(figura, sender) Then
+
+                    MsgBox("Moviendo figura")
+                    Color_Final = sender.BackColor                                                                      'Guardamos el color de la posicion a la que nos movemos
+                    sender.BackColor = Color.Green                                                                      'Seteamos el color de la posicion a verde (creo que se puede prescindir, abajo lo volvemos a poner al default...)
+                    sender.Tag = figura.Tag                                                                             'Establecemos el tag de la imagen a la posicion que hemos pulsado (la figura se mueve y ocupa la posicion vacia)
+                    figura.Tag = "0"                                                                                    'Establecemos la posicion donde estaba la figura a 0 (la figura se mueve y deja libre su posicion inicial)
+                    MsgBox(sender.Tag)
+                    sender.ImageLocation = figura.ImageLocation                                                         'Establecemos la imagen de la posicion vacia a la imagen de la figura que se mueve
+                    figura.ImageLocation = Nothing                                                                      'Quitamos la imagen de la figura de su posicion inicial (null)
+                    figura.BackColor = Color_Inicio                                                                     'Establecemos el color de fondo de la posicion inicial a la del tablero
+                    sender.BackColor = Color_Final                                                                      'Establecemos el color de la posicion a moverse a la del tablero (Creo que se puede prescindir esto junto al verde)
+                    figura = Nothing                                                                                    'Resetamos la figura seleccionada para el siguiente movimiento
+                Else                                                                                                    'No hay figura guardada --> Error --> Pinchar primero una figura
+                    MsgBox("Pincha primero en una figura")
+                End If
+            End If
+
+
+        End If
+
 
 
     End Sub
+
+
+    Function comprobador(ByVal figura As PictureBox, ByVal nCasilla As PictureBox)
+        MsgBox(figura.Tag & "ESTO ES LA FUNCION COMPROBAR")
+        Select Case figura.Tag
+            Case 11
+                Return MovPeon11(figura, nCasilla)
+            Case 12
+                Return True
+            Case 13
+            Case 14
+            Case 15
+            Case 16
+            Case 21
+            Case 22
+            Case 23
+            Case 24
+            Case 25
+            Case 26
+
+
+
+        End Select
+
+
+
+
+    End Function
+
+    Function MovPeon11(ByVal peon As PictureBox, ByVal nuevaCasilla As PictureBox)
+        Dim posInicial As String
+        Dim posFinal As String
+        Dim inicio As String
+
+        posInicial = peon.Name
+        posInicial = posInicial.Substring(7)
+        posFinal = nuevaCasilla.Name
+        posFinal = posFinal.Substring(7)
+        inicio = posInicial.Substring(0, 1)
+
+        MsgBox(posInicial & " ,pos final: " & posFinal)
+
+        If inicio = 6 Then
+            If posFinal = posInicial - 10 Then
+                Return True
+            End If
+            If posFinal = posInicial - 20 Then
+                Return True
+
+
+            End If
+        Else
+            If posFinal = posInicial - 10 Then
+                If nuevaCasilla.Tag > 20 Then
+                    Return False
+                Else
+                    Return True
+                End If
+
+
+            End If
+
+        End If
+
+
+    End Function
+
+
 
 
 
